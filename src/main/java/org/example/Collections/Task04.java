aa6d608013ef4a69ff93a9184f3f01cc76b73c66
PS <112417412+Lutek77@users.noreply.github.com>
2022-12-08 23:54:39 +0100

    Update Task04.java

--- src/main/java/org/example/Collections/Task04.java
+++ src/main/java/org/example/Collections/Task04.java
@@ -1,11 +1,43 @@
-package org.example.Collections;public class Task04 {
+ackage pl.coderslab.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Main04 {
 
     public static void main(String[] args) {
 
+/*        List<City> cities = Main03.initialize();
+        System.out.println("cities = " + cities);
 
-        System.out.println("asascas");
+        List<City> reverseCities = reverse(cities);
+        System.out.println("reverseCities = " + reverseCities);*/
+
+        List<City> cities = new ArrayList<>();
+        cities.add(new City("Warszawa", 1000));
+        cities.add(new City("Iława", 500));
+
+        System.out.println(cities);
+
+        System.out.println(reverse(cities));
 
 
     }
+
+    public static List<City> reverse (List<City> meineList ){
+        List<City> revCities =new ArrayList<>();
+        ListIterator<City> iterator =  meineList.listIterator(meineList.size());
+
+        while (iterator.hasPrevious()) {
+            City c = iterator.previous();
+            revCities.add(c);
+
+        }
+        return  revCities ;
+    }
+
+
+
 
 }
